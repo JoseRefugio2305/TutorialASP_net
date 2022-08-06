ab55cd89d18eb802b678b60e11dde3aa1fcd5bbc
JoseRefugio2305 <joserefugioriveramendoza@gmail.com>
2022-08-05 20:48:22 -0500

    Controlador Secundario

---
+++ TutorialASP_net/Controllers/SecondaryController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TutorialASP_net.Controllers
+{
+    public class SecondaryController : Controller
+    {
+        // GET: Secondary
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SegundaPagina()
+        {
+            return View();
+        }
+    }
+}
